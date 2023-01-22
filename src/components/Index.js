2f71739d6ac3efa919a40b99579ae373f88a946d
Brendan <brendan.oshea@hotmail.co.uk>
2023-01-22 21:05:39 +0000

    again hr import

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {HashRouter as Router, Route, Routes} from 'react-router-dom';
+import {HashRouter, Route, Routes} from 'react-router-dom';
 
 import Navbar from './Navbar';
 import Home from './Home';
