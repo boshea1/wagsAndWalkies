c2a4735d4075a79a6cc703c93b94abdad1ee1e06
Brendan <brendan.oshea@hotmail.co.uk>
2023-01-23 17:47:54 +0000

    change back to the way it was

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
     <div>
     <div>
       <h1 style={{fontSize: '40px',marginBottom: '20px'}}>
-      Contactssss
+      Contact
       </h1>
       <div className='contactgrid'>
         <div className='email'><h1 style={{fontSize: '30px'}}>Email:</h1><h1 style={{fontSize: '20px', marginBottom: '10px'}}>Robbie@email.com</h1></div>
