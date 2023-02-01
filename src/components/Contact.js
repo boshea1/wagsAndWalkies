ee0b338f9229c1d41dd46c4f57a803d285afe181
Brendan <brendan.oshea@hotmail.co.uk>
2023-02-01 18:58:48 +0000

    contact us

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
   return (
     <div className='containercontact'>
       <h1 style={{fontSize: '40px',marginBottom: '20px'}}>
-      Contact
+      Contact us
       </h1>
       <div className='contactgrid'>
         <div className='email'><h1 style={{fontSize: '30px'}}>Email:</h1><h1 style={{fontSize: '20px', marginBottom: '10px'}}>Robbie@email.com</h1></div>
