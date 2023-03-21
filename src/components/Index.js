3580bbd9d9689ec148f818fdba9d8e650c8e16fb
Brendan <brendan.oshea@hotmail.co.uk>
2023-03-21 23:48:25 +0000

    takeaway basename from index

--- src/components/Index.js
+++ src/components/Index.js
@@ -13,7 +13,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Index = () => {
   return (
     <>
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
         <Navbar/>
         <Routes>
             <Route exact path='/' element={<Home/>} />
