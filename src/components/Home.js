75d1f168f621ea79531f2bb3325e873874d4b01b
Brendan <brendan.oshea@hotmail.co.uk>
2023-03-21 22:34:53 +0000

    make change

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <div className='container'>
       <h1 style={{fontSize: '40px', marginBottom: '20px'}}>
-      Home!
+      Home
       </h1>
       <div className='Content'>
         <h3 style={{textAlign: 'center', fontFamily: 'Oswald'}}>We offer professional dog walking and dog sitting</h3>
